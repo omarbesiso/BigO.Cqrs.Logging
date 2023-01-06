059852e9445bb5423cb8b2d29aef256e386bc0f4
Omar Besiso <omar@cloud5.tech>
2023-01-06 13:45:08 +1100

    Initial upload.

---
+++ src/BigO.Cqrs.Logging/LoggingCommandDecorator.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+// ReSharper disable UnusedMember.Global
+
+namespace BigO.Cqrs.Logging;
+
+/// <summary>
+///     Provides a decorator for command handlers that logs entry, exit and errors resulting from executing a command as
+///     well as provides execution elapsed time. This class cannot be inherited.
+/// </summary>
+/// <typeparam name="TCommand">The type of the command.</typeparam>
+/// <seealso cref="ICommandDecorator{TCommand}" />
+public sealed class LoggingCommandDecorator<TCommand> : ICommandDecorator<TCommand>
+    where TCommand : class
+{
+    private readonly ICommandHandler<TCommand> _decorated;
+    private readonly ILogger _logger;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="LoggingCommandDecorator{TCommand}" /> class.
+    /// </summary>
+    /// <param name="decorated">The decorated command handler.</param>
+    /// <param name="logger">The configured logger.</param>
+    public LoggingCommandDecorator(ICommandHandler<TCommand> decorated,
+        ILogger<LoggingCommandDecorator<TCommand>> logger)
+    {
+        _decorated = decorated;
+        _logger = logger;
+    }
+
+    /// <summary>
+    ///     Handles the specified command while applying decorator logic.
+    /// </summary>
+    /// <param name="command">The command to be executed.</param>
+    public async Task Handle(TCommand command)
+    {
+        var commandName = command.GetType().Name;
+
+        _logger.LogInformation($"Start executing command '{commandName}'" + Environment.NewLine);
+
+        var startTime = Stopwatch.GetTimestamp();
+
+        try
+        {
+            await _decorated.Handle(command);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError($"Exception throw while executing command '{commandName}'" +
+                             Environment.NewLine + e.Message + Environment.NewLine);
+            throw;
+        }
+
+        _logger.LogInformation($"Executed command '{commandName}' in {Stopwatch.GetElapsedTime(startTime)}." +
+                               Environment.NewLine);
+    }
+}
---
+++ src/BigO.Cqrs.Logging/LoggingQueryDecorator.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+// ReSharper disable UnusedMember.Global
+
+namespace BigO.Cqrs.Logging;
+
+/// <summary>
+///     Provides a decorator for query handlers that logs entry, exit and errors resulting from processing a query as
+///     well as provides execution elapsed time. This class cannot be inherited.
+/// </summary>
+/// <typeparam name="TQuery">The type of the query.</typeparam>
+/// <typeparam name="TResult">The type of the result.</typeparam>
+/// <seealso cref="IQueryDecorator{TQuery,TResult}" />
+public sealed class LoggingQueryDecorator<TQuery, TResult> : IQueryDecorator<TQuery, TResult>
+    where TQuery : class
+{
+    private readonly IQueryHandler<TQuery, TResult> _decorated;
+    private readonly ILogger<LoggingQueryDecorator<TQuery, TResult>> _logger;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="LoggingQueryDecorator{TQuery,TResult}" /> class.
+    /// </summary>
+    /// <param name="decorated">The decorated query handler.</param>
+    /// <param name="logger">The configured logger.</param>
+    public LoggingQueryDecorator(IQueryHandler<TQuery, TResult> decorated,
+        ILogger<LoggingQueryDecorator<TQuery, TResult>> logger)
+    {
+        _decorated = decorated;
+        _logger = logger;
+    }
+
+    /// <summary>
+    ///     Reads the specified query while applying decorator logic.
+    /// </summary>
+    /// <param name="query">The query.</param>
+    /// <returns>TResult.</returns>
+    public async Task<TResult> Read(TQuery query)
+    {
+        TResult result;
+        var queryName = query.GetType().Name;
+
+        _logger.LogInformation($"Start reading query '{queryName}'" + Environment.NewLine);
+
+        var startTime = Stopwatch.GetTimestamp();
+
+        try
+        {
+            result = await _decorated.Read(query);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError($"Exception throw while reading query '{queryName}'" +
+                             Environment.NewLine + e.Message + Environment.NewLine);
+            throw;
+        }
+
+        _logger.LogInformation($"Executed query '{queryName}' in {Stopwatch.GetElapsedTime(startTime)}." +
+                               Environment.NewLine);
+
+        return result;
+    }
+}
